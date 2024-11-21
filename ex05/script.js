a4a5fa8fae8c9542bce717a017c2c3f8c9224fbe
itmecat2058 <jmorley2007@gmail.com>
2024-11-21 12:08:56 -0500

    Completed task 1 and 2

--- ex05/script.js
+++ ex05/script.js
@@ -2,7 +2,7 @@ const person = {
     "name": "Mr. Bennett",
     "subject": "Chemistry",
     "experience": 15
-}
+};
 
 const teachers = [
     {
@@ -25,17 +25,29 @@ const teachers = [
         "subject": "Chemistry",
         "experience": 15
     },
-]
+];
 
 function task1() {
     // Create your own object assigned to a variable.
     // console.log your object when this function is run.
-}
+    const games = {
+        "name": "Rust",
+        "age": "11 years",
+        "studio": "FacePunch Studios",
+        "players": "Peak 182,603 players"
+    };
+    console.log(games);
+};
 
 function task2() {
     // Write some HTML to create a card display for a person.
     // When you click the button, the data from the `person`
     // object above should be filled in on the page.
+    const personBox = document.querySelector(".person");
+    
+    document.getElementById("name").innerText = "Name: " + person.name
+    document.getElementById("subject").innerText = "Subject: " + person.subject
+    document.getElementById("experience").innerText = "Experience: " + person.experience
 }
 
 function task3() {
